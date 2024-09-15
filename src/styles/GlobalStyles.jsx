82bdf79bc34921520d491f099441983bc4324cde
GwenZi0u0 <a0918566336@gmail.com>
2024-09-16 01:11:35 +0800

    feat globalstyle

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import "normalize.css";
 
 const GlobalStyles = createGlobalStyle`
     * {
@@ -7,6 +8,8 @@ const GlobalStyles = createGlobalStyle`
     
     body {
         font-family: 'Noto Sans', sans-serif;
+        padding: 0;
+        margin: 0;
     }
 `;
 export default GlobalStyles;
