6ae9164b5bd6fb6e81569060f44674e4a1f5a8cd
GwenZi0u0 <a0918566336@gmail.com>
2024-09-16 01:09:25 +0800

    feat allpage

--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
 export default function ProgressPage() {
   return (
-    <>
-      <h1>Progress</h1>
-    </>
+    <Container>
+      <Title>Title</Title>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 80px;
+  background-color: gray;
+  width: 100vw;
+  height: 100px;
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  color: #000000;
+  margin-bottom: 20px;
+`;
