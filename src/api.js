d0b3cf3884b3b1a30fea2f6bd779175865f06c1d
GwenZi0u0 <a0918566336@gmail.com>
2024-09-17 21:17:32 +0800

    feat firebase data api

--- src/api.js
+++ src/api.js
@@ -9,3 +9,39 @@ export const fetchDepartmentsData = async () => {
   }));
   return departments;
 };
+
+export const fetchDoctorsData = async () => {
+  const querySnapshot = await getDocs(collection(fireDb, "doctors"));
+  const doctors = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return doctors;
+};
+
+export const fetchScheduleData = async () => {
+  const querySnapshot = await getDocs(collection(fireDb, "schedules"));
+  const schedules = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return schedules;
+};
+
+export const fetchRegistrationData = async () => {
+  const querySnapshot = await getDocs(collection(fireDb, "registrations"));
+  const registrations = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return registrations;
+};
+
+export const fetchPatientData = async () => {
+  const querySnapshot = await getDocs(collection(fireDb, "patients"));
+  const patients = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return patients;
+};
