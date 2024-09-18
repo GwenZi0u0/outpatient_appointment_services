d127834182d663b1151a9013d383b2a978a877aa
GwenZi0u0 <a0918566336@gmail.com>
2024-09-18 10:49:59 +0800

    feat handleScroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import Logo from "../assets/Logo.svg";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const [isGradient, setIsGradient] = useState(false);
+  const location = useLocation();
+
+  const handleScroll = () => {
+    if (window.scrollY > 80) {
+      console.log("scroll");
+      setIsGradient(true);
+    } else {
+      console.log("no scroll");
+      setIsGradient(false);
+    }
+  };
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      window.addEventListener("scroll", handleScroll);
+    } else {
+      setIsGradient(true);
+    }
+
+    return () => {
+      if (location.pathname === "/") {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [location.pathname]);
+
   return (
     <>
-      <Container>
+      <Container isGradient={isGradient}>
         <LogoLink to="/">
           <LogoIcon src={Logo} alt="Logo" />
         </LogoLink>
@@ -25,11 +53,31 @@ const Container = styled.div`
   position: fixed;
   top: 0;
   display: flex;
-  background-color: transparent;
+  justify-content: space-between;
   height: 80px;
   width: 100vw;
   padding: 0 20px;
   z-index: 10000;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      90deg,
+      #01edd7 0%,
+      #01c7c1 28.72%,
+      #01a6af 57.43%,
+      #01839b 78.72%,
+      #1f7f9a 100%
+    );
+    transition: opacity 0.3s ease;
+    opacity: ${(props) => (props.isGradient ? 1 : 0)};
+    z-index: -1;
+  }
 `;
 
 const LogoLink = styled(Link)`
@@ -37,7 +85,6 @@ const LogoLink = styled(Link)`
   align-items: center;
   justify-content: center;
   height: 100%;
-  padding-right: 351px;
 `;
 const LogoIcon = styled.img`
   width: 226px;
@@ -48,6 +95,7 @@ const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding-right: 40%;
   height: 100%;
   gap: 60px;
 `;
