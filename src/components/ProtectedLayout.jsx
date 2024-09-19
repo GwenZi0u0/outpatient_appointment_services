9c45a7a99241aea6759673ffcc8107e864ff2fad
GwenZi0u0 <a0918566336@gmail.com>
2024-09-19 16:26:31 +0800

    feat logout-fun

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -1,7 +1,29 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
 export default function ProtectedLayout() {
-  return (
-    <>
-      <h1>Protected Layout</h1>
-    </>
-  );
+  const { user, loading, signOut } = useAuth((state) => ({
+    user: state.user,
+    loading: state.loading,
+  }));
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (user) {
+    return (
+      <>
+        <div>
+          <header>
+            <button onClick={signOut}>Sign Out</button>
+          </header>
+          <main></main>
+        </div>
+        <Outlet />
+      </>
+    );
+  }
+
+  return <Navigate to="/login" replace />;
 }
