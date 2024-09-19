84edcc51eac0d238923f8288740cf8441de6bf5b
GwenZi0u0 <a0918566336@gmail.com>
2024-09-19 16:28:26 +0800

    feat login-fun

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,74 @@
 import styled from "styled-components";
 import Lock from "../assets/Lock.svg";
+import { useState, useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signInWithEmail, error } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedEmail =
+      localStorage.getItem("email") || sessionStorage.getItem("email");
+    const savedPassword =
+      localStorage.getItem("password") || sessionStorage.getItem("password");
+
+    if (savedEmail) setEmail(savedEmail);
+    if (savedPassword) setPassword(savedPassword);
+  }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmail(email, password);
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
+
+      navigate("/control-progress");
+    } catch (err) {
+      console.error("Login failed:", err);
+    }
+  };
+
   return (
     <>
-      <Container>
+      <Container onSubmit={handleLogin}>
         <NoticeContainer></NoticeContainer>
         <LoginContainer>
           <LockIcon src={Lock} />
           <LoginContent>
-            <Content>
+            <Content htmlFor="email">
               <LoginIcon></LoginIcon>
-              <LoginInput type="email" placeholder="Login ID" required />
+              <LoginInput
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Login ID"
+                autocomplete="email"
+                required
+              />
             </Content>
-            <Content>
-              <LoginIcon></LoginIcon>
+            <Content htmlFor="password">
+              <LoginIcon />
               <PassWord>
                 password 密 碼
-                <PasswordInput type="password" required />
+                <PasswordInput
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  autocomplete="current-password"
+                  required
+                />
               </PassWord>
             </Content>
-
-            <Button type="submit">登入</Button>
+            <Button type="submit">Sign In</Button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
           </LoginContent>
         </LoginContainer>
       </Container>
@@ -77,20 +124,30 @@ const Content = styled.label`
   justify-content: center;
   width: 100%;
   gap: 20px;
+  border-bottom: 1px solid #e0e0e0;
 `;
 const LoginIcon = styled.div`
   width: 20px;
   height: 20px;
   background-color: #000000;
 `;
-const LoginInput = styled.input``;
+const LoginInput = styled.input`
+  width: 450px;
+  height: 50px;
+  border: none;
+`;
+
+const PasswordInput = styled.input`
+  width: 432px;
+  height: 50px;
+  border: none;
+`;
 
 const PassWord = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
 `;
-const PasswordInput = styled.input``;
 
 const Button = styled.button`
   width: 420px;
