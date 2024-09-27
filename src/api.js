515e63fb9bf46f5cf0169747d37ff18952297e7b
GwenZi0u0 <a0918566336@gmail.com>
2024-09-27 16:01:57 +0800

    refactor api

--- src/api.js
+++ src/api.js
@@ -1,74 +1,39 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, limit } from "firebase/firestore";
 import { fireDb } from "./firebase";
 
-export const fetchDepartmentsData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "departments"));
-  const departments = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return departments;
+const fetchCollectionData = async (collectionName, limitCount = 100) => {
+  try {
+    const q = query(collection(fireDb, collectionName), limit(limitCount));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error(`Error fetching ${collectionName} data:`, error);
+    throw error;
+  }
 };
 
-export const fetchDoctorsData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "doctors"));
-  const doctors = querySnapshot.docs.map((doc) => ({
-    id: doc.uid,
-    ...doc.data(),
-  }));
-  return doctors;
-};
+export const fetchDepartmentsData = () => fetchCollectionData("departments");
+export const fetchDoctorsData = () => fetchCollectionData("doctors");
+export const fetchScheduleData = () => fetchCollectionData("schedules");
+export const fetchRegistrationData = () => fetchCollectionData("registrations");
+export const fetchPatientData = () => fetchCollectionData("patients");
+export const fetchUserData = () => fetchCollectionData("users");
+export const fetchProgressData = () => fetchCollectionData("progress");
+export const fetchRequestLeaveData = () => fetchCollectionData("request_leave");
 
-export const fetchScheduleData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "schedules"));
-  const schedules = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return schedules;
-};
-
-export const fetchRegistrationData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "registrations"));
-  const registrations = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return registrations;
-};
-
-export const fetchPatientData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "patients"));
-  const patients = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return patients;
-};
-
-export const fetchUserData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "users"));
-  const users = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return users;
-};
-
-export const fetchProgressData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "progress"));
-  const progress = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return progress;
-};
-
-export const fetchRequestLeaveData = async () => {
-  const querySnapshot = await getDocs(collection(fireDb, "request_leave"));
-  const requestLeave = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return requestLeave;
+export const fetchDoctorsDataWithLimit = async (limitCount = 50) => {
+  try {
+    const q = query(collection(fireDb, "doctors"), limit(limitCount));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.uid, 
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error fetching doctors data:", error);
+    throw error;
+  }
 };
