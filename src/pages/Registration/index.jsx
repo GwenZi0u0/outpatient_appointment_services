3669a36c817bf9b43d46ed98324ef0fde0f1b251
GwenZi0u0 <a0918566336@gmail.com>
2024-10-02 04:18:03 +0800

    style notice

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -5,10 +5,24 @@ import { fetchDepartmentsData } from "../../api";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import BackGround from "../../assets/background.svg";
+import BackGroundMobile from "../../assets/background_mobile.svg";
 import AnnouncementImg from "../../assets/announcementImage.svg";
 import Loading from "../../assets/loading.gif";
 import CusService from "../../components/CusService";
 import AiRobot from "../../components/AiRobot";
+
+const carouselData = new Map([
+  // [
+  //   "兒童保健暨疫苗接種時間\n週一至週五下午13:30~15:30\n週六上午9:00-10:30\n108年1月起恢復卡介苗接種\n接種時間：每周三下午13:30-14:30",
+  // ],
+  [
+    "門診看診時間\n上午 9:00~12:00 下午 13:30~16:30 夜間 17:30~20:30\n(星期六下午及國定例假日休診.)\n上午診請於11:30前至診間候診、下午診請於16:00前至診間候診、\n夜診請於20:00前至診間候診。",
+  ],
+  // [
+  //   "櫃台掛號開放時間\n上午：8:00~11:00 下午：13:00~16:00 夜間：17:00~20:00",
+  //   "其他掛號方式：\n自動掛號機(依院區門診時間)、網路、電話語音(02-21811995)、\n客服中心(02-25553000)、台北市民當家熱線1999轉888(免付費電話)\n掛號開放時間：00:00~24:00",
+  // ],
+]);
 
 export default function Registration() {
   const { setValue } = useForm();
@@ -54,17 +68,6 @@ export default function Registration() {
   return (
     <MainContainer>
       <Container>
-        <ContainerText>
-          「預約掛號天數為28天內，內容詳見掛號須知。」
-        </ContainerText>
-        <ButtonContainer>
-          <Button $bgcolor="#0267b5de" onClick={handleButtonClick}>
-            網路掛號
-          </Button>
-          <Button $bgcolor="#00b1c1de" onClick={() => navigate("progress")}>
-            看診進度
-          </Button>
-        </ButtonContainer>
         <Carousel>
           <TextCarousel>
             1.
@@ -77,16 +80,53 @@ export default function Registration() {
             21天內有出國旅遊史及職業史、接觸史、群聚史，請主動告知。
           </TextCarousel>
         </Carousel>
+        <ButtonContainer>
+          <Button $bgcolor="#0267b5de" onClick={handleButtonClick}>
+            網路掛號
+          </Button>
+          <Button $bgcolor="#00b1c1de" onClick={() => navigate("progress")}>
+            看診進度
+          </Button>
+        </ButtonContainer>
       </Container>
       <AnnouncementContainer>
         <AnnouncementImage />
-        <Announcement>公告</Announcement>
+        <NoticeContainer>
+          <NoticeTitle style={{ animationDelay: "0s" }}>公告欄</NoticeTitle>
+          <NoticeContent>
+            {Array.from(carouselData).map(([key, value], index) => (
+              <NoticeItem key={index}>
+                <div style={{ marginRight: "50px" }}>
+                  <strong>
+                    {key?.split("\n").map((line, i) => (
+                      <span key={i}>
+                        {line}
+                        <br />
+                      </span>
+                    ))}
+                  </strong>{" "}
+                  {value?.split("\n").map((line, i) => (
+                    <span key={i}>
+                      {line}
+                      <br />
+                    </span>
+                  ))}
+                </div>
+              </NoticeItem>
+            ))}
+          </NoticeContent>
+        </NoticeContainer>
       </AnnouncementContainer>
       <RemindText id="select-region">
-        就醫時請攜帶健保IC卡，未貼照片者請攜帶身分證、駕照、戶口名簿、外籍居留證、敬老卡或身心障礙手冊等證明文件以核對。
+        預約掛號天數為28天內<br />
+        就醫時請攜帶健保IC卡，未貼照片者請攜帶身分證、駕照、戶口名簿、
+        <br />
+        外籍居留證、敬老卡或身心障礙手冊等證明文件以核對。
       </RemindText>
-      <SelectRegion id="select-department">
-        <SelectTitle>掛號、看診進度請選擇以下系別</SelectTitle>
+      <SelectRegion>
+        <SelectTitle id="select-department">
+          掛號、看診進度請選擇以下系別
+        </SelectTitle>
         <SelectCards>
           {data
             .sort((a, b) => a.department_id - b.department_id)
@@ -116,6 +156,59 @@ export default function Registration() {
     </MainContainer>
   );
 }
+
+const NoticeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+  background-color: transparent;
+  gap: 20px;
+  overflow: hidden;
+  @media (max-width: 1024.1px) {
+    width: 100%;
+    padding: 0 30px;
+  }
+`;
+
+const NoticeTitle = styled.div`
+  color: #244a8b;
+  font-size: 28px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  padding: 10px;
+  width: auto;
+`;
+
+const NoticeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding-right: 10px;
+  border-radius: 30px;
+  line-height: 2;
+`;
+
+const NoticeItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  background-color: transparent;
+  border-radius: 30px;
+  padding: 20px;
+  gap: 20px;
+  position: relative;
+
+  &:hover {
+    color: #244a8b;
+    border-color: #b7c3da63;
+  }
+`;
 
 const OverlayContainer = styled.div`
   position: fixed;
@@ -164,40 +257,49 @@ const Container = styled.div`
   height: 100vh;
   min-height: 100vh;
   position: relative;
+  @media (max-width: 1024.1px) {
+    background-position: center;
+    background-size: cover;
+    height: 40vh;
+    min-height: 40vh;
+    background-image: url(${BackGroundMobile});
+  }
 `;
-
-const ContainerText = styled.div`
-  font-size: 30px;
-  letter-spacing: 5px;
-  line-height: 56px;
-  font-weight: 600;
-  width: 100%;
-  height: auto;
-  background-color: #ffffffb5;
-  color: #000000;
-  padding: 0 10px;
-`;
-
 const RemindText = styled.div`
+  text-align: center;
   font-size: 28px;
-  letter-spacing: 5px;
+  letter-spacing: 8px;
   line-height: 56px;
   font-weight: 600;
   width: 100%;
   height: auto;
   background-color: #ffffffb5;
   color: #000000;
-  padding: 100px 185px;
+  padding: 100px 100px;
+  @media (max-width: 1280.1px) {
+    font-size: 24px;
+  }
+  @media (max-width: 1024.1px) {
+    text-align: left;
+    font-size: 22px;
+    padding: 50px 30px;
+    line-height: 1.8;
+  }
 `;
 
 const ButtonContainer = styled.div`
   position: absolute;
-  bottom: 200px;
+  bottom: 160px;
   display: flex;
   flex-direction: row;
   justify-content: center;
   width: 100%;
   gap: 40px;
+  @media (max-width: 1024.1px) {
+    bottom: -100px;
+    gap: 15px;
+    padding: 0 20px;
+  }
 `;
 
 const Button = styled.button`
@@ -209,6 +311,17 @@ const Button = styled.button`
   height: 65px;
   color: #ffffff;
   background-color: ${(props) => props.$bgcolor};
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  @media (max-width: 1024.1px) {
+    width: 50%;
+    height: 90px;
+    border-radius: 10px;
+  }
 `;
 
 const scrollAnimation = keyframes`
@@ -224,20 +337,35 @@ const Carousel = styled.div`
   position: absolute;
   bottom: 20px;
   width: 100%;
-  height: 100px;
+  height: 80px;
   overflow: hidden;
   background-color: rgba(255, 194, 136, 0.7);
   padding-bottom: 20px;
+  @media (max-width: 1024.1px) {
+    height: 60px;
+    bottom: 0;
+  }
+  @media (max-width: 768.1px) {
+    height: 50px;
+  }
 `;
 
 const TextCarousel = styled.div`
   display: inline-block;
-  font-size: 32px;
-  line-height: 100px;
+  font-size: 28px;
+  line-height: 80px;
   font-weight: bold;
   white-space: nowrap;
   color: #b3261e;
-  animation: ${scrollAnimation} 40s linear infinite;
+  animation: ${scrollAnimation} 100s linear infinite;
+  @media (max-width: 1024.1px) {
+    font-size: 22px;
+    line-height: 60px;
+  }
+  @media (max-width: 768.1px) {
+    font-size: 18px;
+    line-height: 50px;
+  }
 `;
 
 const AnnouncementContainer = styled.div`
@@ -245,26 +373,24 @@ const AnnouncementContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding: 90px 0 100px 0;
+  padding-top: 90px;
   background-color: transparent;
   width: 100%;
-  height: 100vh;
-  gap: 200px;
+  height: auto;
+  gap: 100px;
+  @media (max-width: 1024.1px) {
+    padding-top: 130px;
+  }
 `;
 const AnnouncementImage = styled.div`
   width: 348px;
   height: 550px;
+  margin: 0 100px;
   background-color: #ffffff;
   background-image: url(${AnnouncementImg});
-`;
-
-const Announcement = styled.div`
-  width: 837px;
-  height: 736px;
-  background-color: #f2f2f2;
-  color: #000000;
-  display: flex;
-  flex-direction: column;
+  @media (max-width: 1024.1px) {
+    display: none;
+  }
 `;
 
 const SelectRegion = styled.div`
@@ -275,6 +401,19 @@ const SelectRegion = styled.div`
   margin-top: 10px;
   padding: 20px 165px 100px;
   gap: 40px;
+  @media (max-width: 1280.1px) {
+    padding: 20px 70px 100px;
+  }
+  @media (max-width: 1024.1px) {
+    padding: 20px 30px 100px;
+  }
+  @media (max-width: 768.1px) {
+    padding: 20px 30px 100px;
+    gap: 20px;
+  }
+  @media (max-width: 480.1px) {
+    padding: 20px 10px 100px;
+  }
 `;
 
 const SelectTitle = styled.div`
@@ -283,6 +422,9 @@ const SelectTitle = styled.div`
   color: #000000;
   width: 100%;
   background-color: transparent;
+  @media (max-width: 1024.1px) {
+    font-size: 28px;
+  }
 `;
 
 const SelectCards = styled.div`
@@ -290,10 +432,17 @@ const SelectCards = styled.div`
   grid-template-columns: repeat(3, 1fr);
   background-color: #ffffff;
   gap: 40px;
-  ${(props) =>
-    props.$specialty &&
-    `
-  `}
+  @media (max-width: 1024.1px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 30px;
+  }
+  @media (max-width: 768.1px) {
+    gap: 20px;
+  }
+  @media (max-width: 480.1px) {
+    grid-template-columns: repeat(1, 1fr);
+    gap: 10px;
+  }
 `;
 
 export const SelectCard = styled.button`
@@ -310,6 +459,18 @@ export const SelectCard = styled.button`
     transform: scale(1.1);
     box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
   }
+  @media (max-width: 1440.1px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
+  @media (max-width: 768.1px) {
+    padding: 10px;
+  }
+  @media (max-width: 480.1px) {
+    position: relative;
+    width: 80%;
+  }
 `;
 
 export const ContentContainer = styled.div`
@@ -317,12 +478,22 @@ export const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  @media (max-width: 480.1px) {
+    position: absolute;
+    align-items: center;
+    width: 100%;
+    top: 90px;
+    left: 0;
+  }
 `;
 
 export const SelectCardTitle = styled.span`
   font-size: 24px;
   color: #1e1e1e;
   margin-bottom: 10px;
+  @media (max-width: 480.1px) {
+    color: #ffffff;
+  }
 `;
 
 export const SelectCardImage = styled.img`
@@ -331,6 +502,18 @@ export const SelectCardImage = styled.img`
   width: 160px;
   height: 160px;
   background-color: lightgray;
+  border-radius: 10px;
+  @media (max-width: 1440.1px) {
+    margin-right: 0;
+  }
+  @media (max-width: 768.1px) {
+    margin-right: 0;
+  }
+  @media (max-width: 480.1px) {
+    background-color: #000000;
+    width: 200px;
+    height: 200px;
+  }
 `;
 
 const SelectContent = styled.span`
@@ -338,4 +521,7 @@ const SelectContent = styled.span`
   font-size: 16px;
   color: #757575;
   line-height: 1.5;
+  @media (max-width: 768.1px) {
+    display: none;
+  }
 `;
