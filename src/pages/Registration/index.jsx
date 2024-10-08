df0c99cf85389f12d0326f01ffc9fa75d1c69f59
GwenZi0u0 <a0918566336@gmail.com>
2024-10-08 23:26:02 +0800

    feat loading settimeout

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,13 +1,13 @@
 import styled, { keyframes } from "styled-components";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchDepartmentsData } from "../../api";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import BackGround from "../../assets/background.svg";
 import BackGroundMobile from "../../assets/background_mobile.svg";
 import AnnouncementImg from "../../assets/announcementImage.svg";
-import Loading from "../../assets/loading.gif";
+import Loading from "../../components/Loading";
 import CusService from "../../components/CusService";
 import AiRobot from "../../components/AiRobot";
 
@@ -18,33 +18,74 @@ const carouselData = new Map([
   [
     "門診看診時間\n上午 9:00~12:00 下午 13:30~16:30 夜間 17:30~20:30\n(星期六下午及國定例假日休診.)\n上午診請於11:30前至診間候診、下午診請於16:00前至診間候診、\n夜診請於20:00前至診間候診。",
   ],
-  // [
-  //   "櫃台掛號開放時間\n上午：8:00~11:00 下午：13:00~16:00 夜間：17:00~20:00",
-  //   "其他掛號方式：\n自動掛號機(依院區門診時間)、網路、電話語音(02-21811995)、\n客服中心(02-25553000)、台北市民當家熱線1999轉888(免付費電話)\n掛號開放時間：00:00~24:00",
-  // ],
+  [
+    "櫃台掛號開放時間\n上午：8:00~11:00 下午：13:00~16:00 夜間：17:00~20:00",
+    // "其他掛號方式：\n自動掛號機(依院區門診時間)、網路、電話語音(02-21811995)、\n客服中心(02-25553000)、台北市民當家熱線1999轉888(免付費電話)\n掛號開放時間：00:00~24:00",
+  ],
 ]);
 
 export default function Registration() {
   const { setValue } = useForm();
+  const location = useLocation();
+  const shouldScrollRef = useRef(false);
   const navigate = useNavigate();
   const [showAI, setShowAI] = useState(false);
   const [userQuestion, setUserQuestion] = useState("");
-  const { data, isLoading } = useQuery({
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingProgress, setLoadingProgress] = useState(0);
+  const { data, isLoading: isDataLoading } = useQuery({
     queryKey: ["departments"],
     queryFn: fetchDepartmentsData,
   });
+
+  useEffect(() => {
+    if (location.state?.scrollToRegion && !shouldScrollRef.current) {
+      shouldScrollRef.current = true;
+      const element = document.getElementById("select-region");
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        });
+      }
+    }
+  }, [location]);
+
 
   const handleCardClick = (department) => {
     setValue("department", department);
     navigate("/appointment", { state: { department } });
   };
 
-  if (isLoading) {
-    return (
-      <LoadingContainer>
-        <LoadingGif src={Loading} alt="載入中..." />
-      </LoadingContainer>
-    );
+  useEffect(() => {
+    const simulateLoading = () => {
+      const interval = setInterval(() => {
+        setLoadingProgress((prevProgress) => {
+          if (prevProgress >= 100) {
+            clearInterval(interval);
+            setTimeout(() => {
+              setLoadingProgress(100);
+              setTimeout(() => {
+                setIsLoading(false);
+              }, 180);
+            }, 180);
+            return 100;
+          }
+          return prevProgress + 10;
+        });
+      }, 450);
+
+      return interval;
+    };
+
+    const interval = simulateLoading();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (isLoading || isDataLoading) {
+    return <Loading isLoading={true} progress={loadingProgress} />;
   }
 
   const handleButtonClick = () => {
@@ -95,7 +136,6 @@ export default function Registration() {
         </ButtonContainer>
       </Container>
       <AnnouncementContainer>
-        <AnnouncementImage />
         <NoticeContainer>
           <NoticeTitle style={{ animationDelay: "0s" }}>公告欄</NoticeTitle>
           <NoticeContent>
@@ -121,6 +161,7 @@ export default function Registration() {
             ))}
           </NoticeContent>
         </NoticeContainer>
+        <AnnouncementImage />
       </AnnouncementContainer>
       <RemindText id="select-region">
         預約掛號天數為28天內
@@ -173,7 +214,6 @@ const NoticeContainer = styled.div`
   overflow: hidden;
   @media (max-width: 1024.1px) {
     width: 100%;
-    padding: 0 30px;
   }
 `;
 
@@ -206,7 +246,7 @@ const NoticeItem = styled.div`
   letter-spacing: 1px;
   background-color: transparent;
   border-radius: 30px;
-  padding: 20px;
+  padding: 10px;
   gap: 20px;
   position: relative;
 
@@ -227,19 +267,6 @@ const OverlayContainer = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 20000;
-`;
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const LoadingGif = styled.img`
-  width: 200px;
-  height: 200px;
-  object-fit: cover;
 `;
 
 const MainContainer = styled.div`
@@ -288,8 +315,12 @@ const RemindText = styled.div`
   @media (max-width: 1024.1px) {
     text-align: left;
     font-size: 22px;
-    padding: 50px 30px;
+    padding: 50px 72px;
     line-height: 1.8;
+  }
+  @media (max-width: 768.1px) {
+    font-size: 20px;
+    padding: 40px 50px;
   }
 `;
 
@@ -379,19 +410,23 @@ const AnnouncementContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding-top: 90px;
   background-color: transparent;
   width: 100%;
   height: auto;
-  gap: 100px;
+  gap: 0px;
+  padding: 90px 100px 0;
   @media (max-width: 1024.1px) {
-    padding-top: 130px;
+    padding: 130px 50px 0;
+  }
+  @media (max-width: 768.1px) {
+    padding: 130px 30px 0;
   }
 `;
 const AnnouncementImage = styled.div`
   width: 348px;
   height: 550px;
   margin: 0 100px;
+  border-radius: 30px;
   background-color: #ffffff;
   background-image: url(${AnnouncementImg});
   @media (max-width: 1024.1px) {
@@ -430,6 +465,9 @@ const SelectTitle = styled.div`
   background-color: transparent;
   @media (max-width: 1024.1px) {
     font-size: 28px;
+  }
+  @media (max-width: 480.1px) {
+    text-align: center;
   }
 `;
 
@@ -471,6 +509,7 @@ export const SelectCard = styled.button`
     gap: 10px;
   }
   @media (max-width: 768.1px) {
+    width: 90%;
     padding: 10px;
   }
   @media (max-width: 480.1px) {
