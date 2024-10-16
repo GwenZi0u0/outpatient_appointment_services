63597c34d2a20e117ad32e9b86f9605f903aabd6
GwenZi0u0 <a0918566336@gmail.com>
2024-10-16 19:05:58 +0800

    refactor: Pull the router out for independent management.=>routes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/layout/Header";
-import ProtectedLayout from "./components/layout/ProtectedLayout";
+import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import AppointmentPage from "./pages/Appointment";
-import CancelRegistrationPage from "./pages/CancelRegistration";
-import ClassSchedulePage from "./pages/ClassSchedule";
-import ControlProgressPage from "./pages/ControlProgress";
-import DoctorProfilePage from "./pages/DoctorProfile";
-import LoginPage from "./pages/Login";
-import NotFoundPage from "./pages/NotFound";
-import ProgressPage from "./pages/Progress";
-import RegistrationPage from "./pages/Registration";
+import { router } from "./routes";
 import GlobalStyles from "./styles/GlobalStyles";
 
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    element: <Header />,
-    children: [
-      {
-        path: "/",
-        element: <RegistrationPage />,
-      },
-      {
-        path: "appointment",
-        element: <AppointmentPage />,
-      },
-      {
-        path: "progress",
-        element: <ProgressPage />,
-      },
-      {
-        path: "cancel-registration",
-        element: <CancelRegistrationPage />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-  {
-    path: "doctor-profile/:doctorId",
-    element: <DoctorProfilePage />,
-  },
-  {
-    element: <ProtectedLayout />,
-    children: [
-      {
-        path: "control-progress",
-        element: <ControlProgressPage />,
-      },
-      {
-        path: "class-schedule",
-        element: <ClassSchedulePage />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />,
-  },
-]);
 
 export default function App() {
   return (
